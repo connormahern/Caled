e936d68cc8e8c99ec3dcc992b9e3aa85b95efbc4
Connor Mahern <cjmahern@iu.edu>
2021-03-01 20:29:49 -0500

    Caled Push2

--- project/__init__.py
+++ project/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
+
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
