9c90116805a3a177d5761d78764e2f9ce46d9a27
Connor Mahern <cjmahern@iu.edu>
2021-03-14 17:26:06 -0400

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import requests
-
-response = requests.get('https://httpbin.org/ip')
-
-print('Your IP is {0}'.format(response.json()['origin']))
+from flask import Flask 
+  
+app = Flask(__name__) 
+  
+@app.route("/") 
+def home_view():
+	return "<h1> Caled </h1>" 
