0b8f8f5ad5ff7b5bd661026eefebb6a4fd4389bb
Connor Mahern <cjmahern@iu.edu>
2021-03-15 16:40:04 -0400

    Fixing Wgsi

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
-from app.config import configure_app
+from main.config import configure_app
  
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app.main import app
+from main.__init__ import create_app
 
 if __name__ == "__main__" :
-	app.run()
+	create_app()
