39ab263c0d067150ddd2ccf2bad3ee042c39e339
Connor Mahern <cjmahern@iu.edu>
2021-03-15 19:11:57 -0400

    adding main in init

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,6 @@
-from Flask import Flask
-from Flask_sqlalchemy import SQLAlchemy
-from Flask_login import LoginManager
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from main.config import configure_app
 from main import app
  
--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
 
-from Flask import Blueprint, render_template, redirect, url_for, request, flash, session, jsonify, Flask
-from Flask_login import login_required, current_user
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session, jsonify, Flask
+from flask_login import login_required, current_user
 from .models import Course, User
-from Flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 main = Blueprint('main', __name__)
 #engine = create_engine('sqlite:///db.sqlite')
