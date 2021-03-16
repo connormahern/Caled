425ba7f9e134f2849a2c660761af5a453c3ccca8
Connor Mahern <cjmahern@iu.edu>
2021-03-15 21:13:40 -0400

    Library Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.instance.config import app_config
+from instance.config import app_config
 from flask_migrate import Migrate
 import os
 
