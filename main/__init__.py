f3e1b5744eaddf37dbce29628e9b2c6f275d23c6
connormahern <cjmahern@iu.edu>
2021-03-28 15:31:05 -0400

    Db create all removal

--- main/__init__.py
+++ main/__init__.py
@@ -28,9 +28,9 @@ def create_app(config_name):
     
     from .models import User
 
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
+    # @app.before_first_request
+    # def create_tables():
+    #     db.create_all()
     
     @login_manager.user_loader
     def load_user(user_id):
